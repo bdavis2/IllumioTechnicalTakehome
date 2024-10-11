227cc858313a2a24b5d06b6a2b4ea9da511643b2
slayerx117 <betacosmhgeeliekabaws@gmail.com>
2024-10-10 17:27:23 -0700

    remove testing code

--- VPCLogParser.py
+++ VPCLogParser.py
@@ -1,71 +1,11 @@
 import argparse
 
-# sample flow log:
-test_log_str = """
-version,account-id,interface-id,srcaddr,dstaddr,srcport,dstport,protocol,packets,bytes,start,end,action,log-status
-2,123456789012,eni-0a1b2c3d,10.0.1.201,198.51.100.2,443,49153,6,25,20000,1620140761,1620140821,ACCEPT,OK
-2,123456789012,eni-4d3c2b1a,192.168.1.100,203.0.113.101,23,49154,6,15,12000,1620140761,1620140821,REJECT,OK
-2,123456789012,eni-5e6f7g8h,192.168.1.101,198.51.100.3,25,49155,6,10,8000,1620140761,1620140821,ACCEPT,OK
-2,123456789012,eni-9h8g7f6e,172.16.0.100,203.0.113.102,110,49156,6,12,9000,1620140761,1620140821,ACCEPT,OK
-2,123456789012,eni-7i8j9k0l,172.16.0.101,192.0.2.203,993,49157,6,8,5000,1620140761,1620140821,ACCEPT,OK
-2,123456789012,eni-6m7n8o9p,10.0.2.200,198.51.100.4,143,49158,6,18,14000,1620140761,1620140821,ACCEPT,OK
-2,123456789012,eni-1a2b3c4d,192.168.0.1,203.0.113.12,1024,80,6,10,5000,1620140661,1620140721,ACCEPT,OK
-2,123456789012,eni-1a2b3c4d,203.0.113.12,192.168.0.1,80,1024,6,12,6000,1620140661,1620140721,ACCEPT,OK
-2,123456789012,eni-1a2b3c4d,10.0.1.102,172.217.7.228,1030,443,6,8,4000,1620140661,1620140721,ACCEPT,OK
-2,123456789012,eni-5f6g7h8i,10.0.2.103,52.26.198.183,56000,23,6,15,7500,1620140661,1620140721,REJECT,OK
-2,123456789012,eni-9k10l11m,192.168.1.5,51.15.99.115,49321,25,6,20,10000,1620140661,1620140721,ACCEPT,OK
-2,123456789012,eni-1a2b3c4d,192.168.1.6,87.250.250.242,49152,110,6,5,2500,1620140661,1620140721,ACCEPT,OK
-2,123456789012,eni-2d2e2f3g,192.168.2.7,77.88.55.80,49153,993,6,7,3500,1620140661,1620140721,ACCEPT,OK
-2,123456789012,eni-4h5i6j7k,172.16.0.2,192.0.2.146,49154,143,6,9,4500,1620140661,1620140721,ACCEPT,OK
-"""
-
-
-
 class FlowLogLine:
     def __init__(self, fields: list[str], *args):
-        # self.version = args[0]
-        # self.account_id = args[1]
-        # self.interface_id = args[2]
-        # self.srcaddr = args[3]
-        # self.distaddr = args[4]
-        # self.srcport = args[5]
         self.dstport = args[fields.index("dstport")]
         self.protocol = args[fields.index("protocol")]
-        # self.packets = args[8]
-        # self._bytes = args[9]
-        # self.start = args[10]
-        # self.end = args[11]
-        # self.action = args[12]
         self.log_status = args[fields.index("log-status")]
 
-
-# lookup table
-test_table_str = """
-25,tcp,sv_P1
-68,udp,sv_P2
-23,tcp,sv_P1
-31,udp,SV_P3
-443,tcp,sv_P2
-22,tcp,sv_P4
-3389,tcp,sv_P5
-0,icmp,sv_P5
-110,tcp,email
-993,tcp,email
-143,tcp,email"""
-
-
-# dstport,protocol,tag
-# 25,tcp,sv_P1
-# 68,udp,sv_P2
-# 23,tcp,sv_P1
-# 31,udp,SV_P3
-# 443,tcp,sv_P2
-# 22,tcp,sv_P4
-# 3389,tcp,sv_P5
-# 0,icmp,sv_P5
-# 110,tcp,email
-# 993,tcp,email
-# 143,tcp,email
 
 class LookupTableRow:
     def __init__(self, *args):
@@ -173,4 +113,3 @@ if __name__ == '__main__':
         for k, v in ppc_counts.items():
             out.write('%s,%s,%s\n' % (k[0], k[1], v))
         out.close()
-
